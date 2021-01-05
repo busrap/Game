3acda1f281aa1106ca39ec60ade2e1581c03cda6
busra06 <busra.polat.tr@gmail.com>
2021-01-05 13:59:39 +0300

    game

---
+++ SehirTahmin/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class oyun
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_OyunBaslat = new System.Windows.Forms.Button();
+            this.txt_Tahmin = new System.Windows.Forms.TextBox();
+            this.btn_HarfGir = new System.Windows.Forms.Button();
+            this.grpBox_control = new System.Windows.Forms.GroupBox();
+            this.btn_yeni_oyun = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_dahaOnceGirilen_harf = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_kelime_tahmin = new System.Windows.Forms.Button();
+            this.txt_kelime_tahmin = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_kalan_hak = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_OyunBaslat
+            // 
+            this.btn_OyunBaslat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_OyunBaslat.ForeColor = System.Drawing.Color.DarkRed;
+            this.btn_OyunBaslat.Location = new System.Drawing.Point(264, 344);
+            this.btn_OyunBaslat.Name = "btn_OyunBaslat";
+            this.btn_OyunBaslat.Size = new System.Drawing.Size(175, 38);
+            this.btn_OyunBaslat.TabIndex = 0;
+            this.btn_OyunBaslat.Text = "OYUN BAŞLASIN !";
+            this.btn_OyunBaslat.UseVisualStyleBackColor = true;
+            this.btn_OyunBaslat.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_Tahmin
+            // 
+            this.txt_Tahmin.Location = new System.Drawing.Point(214, 39);
+            this.txt_Tahmin.Multiline = true;
+            this.txt_Tahmin.Name = "txt_Tahmin";
+            this.txt_Tahmin.Size = new System.Drawing.Size(203, 23);
+            this.txt_Tahmin.TabIndex = 2;
+            // 
+            // btn_HarfGir
+            // 
+            this.btn_HarfGir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_HarfGir.ForeColor = System.Drawing.Color.DarkRed;
+            this.btn_HarfGir.Location = new System.Drawing.Point(214, 65);
+            this.btn_HarfGir.Name = "btn_HarfGir";
+            this.btn_HarfGir.Size = new System.Drawing.Size(203, 38);
+            this.btn_HarfGir.TabIndex = 3;
+            this.btn_HarfGir.Text = "Harf gir";
+            this.btn_HarfGir.UseVisualStyleBackColor = true;
+            this.btn_HarfGir.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // grpBox_control
+            // 
+            this.grpBox_control.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.grpBox_control.Location = new System.Drawing.Point(53, 182);
+            this.grpBox_control.Name = "grpBox_control";
+            this.grpBox_control.Size = new System.Drawing.Size(1253, 140);
+            this.grpBox_control.TabIndex = 4;
+            this.grpBox_control.TabStop = false;
+            this.grpBox_control.Text = "GİZLENEN ŞEHRİ TAHMİN EDİNİZ";
+            // 
+            // btn_yeni_oyun
+            // 
+            this.btn_yeni_oyun.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_yeni_oyun.ForeColor = System.Drawing.Color.DarkRed;
+            this.btn_yeni_oyun.Location = new System.Drawing.Point(53, 344);
+            this.btn_yeni_oyun.Name = "btn_yeni_oyun";
+            this.btn_yeni_oyun.Size = new System.Drawing.Size(188, 38);
+            this.btn_yeni_oyun.TabIndex = 5;
+            this.btn_yeni_oyun.Text = "YENİ OYUN";
+            this.btn_yeni_oyun.UseVisualStyleBackColor = true;
+            this.btn_yeni_oyun.Click += new System.EventHandler(this.btn_yeni_oyun_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(713, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(236, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Daha önce kullanılan harfler =>\r\n";
+            // 
+            // lbl_dahaOnceGirilen_harf
+            // 
+            this.lbl_dahaOnceGirilen_harf.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_dahaOnceGirilen_harf.Location = new System.Drawing.Point(969, 31);
+            this.lbl_dahaOnceGirilen_harf.Name = "lbl_dahaOnceGirilen_harf";
+            this.lbl_dahaOnceGirilen_harf.Size = new System.Drawing.Size(210, 148);
+            this.lbl_dahaOnceGirilen_harf.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(84, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Bir harf giriniz :";
+            // 
+            // btn_kelime_tahmin
+            // 
+            this.btn_kelime_tahmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_kelime_tahmin.ForeColor = System.Drawing.Color.DarkRed;
+            this.btn_kelime_tahmin.Location = new System.Drawing.Point(437, 65);
+            this.btn_kelime_tahmin.Name = "btn_kelime_tahmin";
+            this.btn_kelime_tahmin.Size = new System.Drawing.Size(199, 38);
+            this.btn_kelime_tahmin.TabIndex = 9;
+            this.btn_kelime_tahmin.Text = "Kelimeyi tahmin et";
+            this.btn_kelime_tahmin.UseVisualStyleBackColor = true;
+            this.btn_kelime_tahmin.Click += new System.EventHandler(this.btn_kelime_tahmin_Click);
+            // 
+            // txt_kelime_tahmin
+            // 
+            this.txt_kelime_tahmin.Location = new System.Drawing.Point(437, 39);
+            this.txt_kelime_tahmin.Multiline = true;
+            this.txt_kelime_tahmin.Name = "txt_kelime_tahmin";
+            this.txt_kelime_tahmin.Size = new System.Drawing.Size(199, 23);
+            this.txt_kelime_tahmin.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(260, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 24);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "KALAN HAK SAYISI :";
+            // 
+            // lbl_kalan_hak
+            // 
+            this.lbl_kalan_hak.AutoSize = true;
+            this.lbl_kalan_hak.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_kalan_hak.Location = new System.Drawing.Point(487, 122);
+            this.lbl_kalan_hak.Name = "lbl_kalan_hak";
+            this.lbl_kalan_hak.Size = new System.Drawing.Size(30, 31);
+            this.lbl_kalan_hak.TabIndex = 12;
+            this.lbl_kalan_hak.Text = "4";
+            // 
+            // oyun
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PowderBlue;
+            this.ClientSize = new System.Drawing.Size(1341, 396);
+            this.Controls.Add(this.lbl_kalan_hak);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_kelime_tahmin);
+            this.Controls.Add(this.btn_kelime_tahmin);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbl_dahaOnceGirilen_harf);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_yeni_oyun);
+            this.Controls.Add(this.grpBox_control);
+            this.Controls.Add(this.btn_HarfGir);
+            this.Controls.Add(this.txt_Tahmin);
+            this.Controls.Add(this.btn_OyunBaslat);
+            this.Name = "oyun";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.oyun_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_OyunBaslat;
+        private System.Windows.Forms.TextBox txt_Tahmin;
+        private System.Windows.Forms.Button btn_HarfGir;
+        private System.Windows.Forms.GroupBox grpBox_control;
+        private System.Windows.Forms.Button btn_yeni_oyun;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_dahaOnceGirilen_harf;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_kelime_tahmin;
+        private System.Windows.Forms.TextBox txt_kelime_tahmin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_kalan_hak;
+    }
+}
+
---
+++ SehirTahmin/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class oyun : Form
+    {
+        public oyun()
+        {
+            InitializeComponent();
+        }
+        string[] sehirler = { "İstanbul", "Ankara", "İzmir", "Adana", "Adıyaman",
+                    "Afyonkarahisar", "Ağrı", "Aksaray", "Amasya", "Antalya",
+                    "Ardahan", "Artvin", "Aydın", "Balıkesir", "Bartın", "Batman",
+                    "Bayburt", "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur",
+                    "Bursa", "Çanakkale", "Çankırı", "Çorum", "Denizli",
+                    "Diyarbakır", "Düzce", "Edirne", "Elazığ", "Erzincan",
+                    "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane",
+                    "Hakkari", "Hatay", "Iğdır", "Isparta", "Kahramanmaraş",
+                    "Karabük", "Karaman", "Kars", "Kastamonu", "Kayseri",
+                    "Kırıkkale", "Kırklareli", "Kırşehir", "Kilis", "Kocaeli",
+                    "Konya", "Kütahya", "Malatya", "Manisa", "Mardin", "Mersin",
+                    "Muğla", "Muş", "Nevşehir", "Niğde", "Ordu", "Osmaniye", "Rize",
+                    "Sakarya", "Samsun", "Siirt", "Sinop", "Sivas", "Şırnak",
+                    "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Şanlıurfa", "Uşak",
+                    "Van", "Yalova", "Yozgat", "Zonguldak"};
+        int sayi;
+        int tamamiBilindi = 0;
+        int kalanHakSayisi = 4;
+        private void oyun_Load(object sender, EventArgs e)
+        {
+            btn_kelime_tahmin.Enabled = false;
+            btn_HarfGir.Enabled = false;
+            btn_yeni_oyun.Enabled = false;
+        }
+        private void button1_Click(object sender, EventArgs e) // OYUN BAŞLAT
+        {
+            btn_kelime_tahmin.Enabled = true;
+            btn_HarfGir.Enabled = true;
+            btn_OyunBaslat.Enabled = false;
+
+            Random rnd = new Random();
+
+            int b = 5;
+
+            sayi = rnd.Next(0, sehirler.Length);
+            foreach (char w in sehirler[sayi])
+            {
+
+                Label lbl = new Label();
+                lbl.Text = w.ToString();
+                lbl.Top = 35;
+                lbl.Left = b * 20;
+                lbl.Width = 40;
+                lbl.Height = 30;
+                lbl.BackColor = Color.HotPink;
+                lbl.ForeColor = Color.HotPink;
+                lbl.Font = new Font("Custom", 13, FontStyle.Bold);
+                grpBox_control.Controls.Add(lbl);
+                b += 3;
+            }
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e) // HARF GİR
+        {
+
+
+
+
+            if (txt_Tahmin.Text.Length != 1)
+            {
+                MessageBox.Show("UYARI: Lütfen bir harf girişi yapınız!");
+                txt_Tahmin.Text = "";
+                return;
+
+            }
+            else if (lbl_dahaOnceGirilen_harf.Text.Contains(txt_Tahmin.Text))
+            {
+                MessageBox.Show("UYARI:Aynı harfi daha önce denedin!");
+                txt_Tahmin.Text = "";
+                txt_Tahmin.Focus();
+                kalanHakSayisi--;
+                lbl_kalan_hak.Text = kalanHakSayisi.ToString();
+                if (kalanHakSayisi <= 0)
+                {
+
+                    lbl_kalan_hak.Text = "Hak saysı bitti! Kaybettiniz!";
+
+                    btn_HarfGir.Visible = false;
+                    btn_kelime_tahmin.Visible = false;
+                    btn_yeni_oyun.Enabled = true;
+                }
+            }
+
+            lbl_dahaOnceGirilen_harf.Text += txt_Tahmin.Text + " ";
+
+
+            bool arti = false;
+            foreach (Control tool in grpBox_control.Controls)
+            {
+                if (tool is Label)
+                {
+                    Label label = tool as Label;
+                    if (txt_Tahmin.Text.ToUpper() == label.Text.ToUpper())
+                    {
+                        label.BackColor = Color.Yellow;
+                        label.ForeColor = Color.Black;
+                        arti = true;
+                        tamamiBilindi++;
+                    }
+                    if (tamamiBilindi == sehirler[sayi].Length)
+                    {
+                        MessageBox.Show("Tüm harfleri doğru tahmin ettin ve kazandın!");
+                        btn_kelime_tahmin.Enabled = false;
+                        btn_HarfGir.Enabled = false;
+                        btn_OyunBaslat.Enabled = false;
+                        btn_yeni_oyun.Enabled = true;
+                        tamamiBilindi = 0;
+                    }
+                  
+                }
+
+            }
+            if (arti == false)
+            {
+                kalanHakSayisi--;
+                lbl_kalan_hak.Text = kalanHakSayisi.ToString();
+                if (kalanHakSayisi<=0)
+                {
+                    lbl_kalan_hak.ForeColor = Color.Red;
+                    lbl_kalan_hak.Text = "Ha ha ha Kaybettin !";
+                    btn_HarfGir.Visible = false;
+                    btn_kelime_tahmin.Visible = false;
+                    btn_yeni_oyun.Enabled = true;
+
+                }
+            }
+
+
+            txt_Tahmin.Text = "";
+        }
+
+        private void btn_yeni_oyun_Click(object sender, EventArgs e)
+        {
+            grpBox_control.Controls.Clear();
+            btn_HarfGir.Visible = true;
+            kalanHakSayisi = 4;
+            lbl_kalan_hak.Text = "4";
+            btn_kelime_tahmin.Visible = true;
+            btn_OyunBaslat.Enabled = true;
+            txt_Tahmin.Text = "";
+            lbl_dahaOnceGirilen_harf.Text = "";
+            txt_Tahmin.Focus();
+            btn_yeni_oyun.Enabled = false;
+            btn_kelime_tahmin.Enabled = false;
+            btn_HarfGir.Enabled = false;
+            btn_yeni_oyun.Enabled = false;
+
+        }
+
+
+        private void btn_kelime_tahmin_Click(object sender, EventArgs e)
+        {
+            if (txt_kelime_tahmin.Text.ToUpper() == sehirler[sayi].ToUpper())
+            {
+                MessageBox.Show("Tebrikler zınk diye bildin!");
+               
+                txt_Tahmin.Text = "";
+                txt_kelime_tahmin.Text = "";
+                btn_kelime_tahmin.Enabled = false;
+                btn_HarfGir.Enabled = false;
+                btn_OyunBaslat.Enabled = false;
+                btn_yeni_oyun.Enabled = true;
+                lbl_kalan_hak.Text = "";
+                txt_Tahmin.Focus();
+                lbl_kalan_hak.Text = "Tebrikler nokta atışı yaptın ve kazandın!";
+            }
+            else
+            {
+                MessageBox.Show("Kötü haber, bilemedin ve kaybettin!");
+                
+                txt_kelime_tahmin.Text = "";
+                txt_Tahmin.Text = "";
+                lbl_kalan_hak.Text = "";
+                btn_kelime_tahmin.Enabled = false;
+                btn_HarfGir.Enabled = false;
+                btn_OyunBaslat.Enabled = false;
+                btn_yeni_oyun.Enabled = true;
+
+            }
+        }
+
+
+
+    }
+}
